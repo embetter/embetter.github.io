797f7181abf3faea72c8d97aab3151403f6e3eab
VinayakGhosh <r.vinayakghosh@gmail.com>
2024-06-29 03:29:01 +0530

    Included responsiveness to homepage and services

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./layouts/**/*.html",
-    "./content/**/*.md", "./layouts/*.html"],
+    "./content/**/*.md", "./layouts/*.{html,js}"],
   theme: {
     extend: {},
   },
